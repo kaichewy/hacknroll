55eb3fbca61ecb3647cc2d202be6e20222efa5b8
jolenetan1234 <jolneetanky@gmail.com>
2025-01-18 12:58:04 +0800

    [frontend] set up api client and basic types

---
+++ frontend/src/api/mock-map-api.ts
@@ -0,0 +1,18 @@
+// THIS IS THE MOCK API CONTROLLER.
+// LATER WE WILL ABSTRACT THIS OUT INTO BACKEND.
+// FOR NOW API CALLS (TO GOOGLE MAPS) WILL BE DONE DIRECTLY FROM HERE
+
+import { ApiResponse } from "./types";
+
+class MockMapApi {
+    async getRoute(): Promise<ApiResponse<any>> {
+        return {
+            status: "success",
+            data: "",
+            error: "",
+        };
+    }
+}
+
+const mockMapApi = new MockMapApi();
+export default mockMapApi;
---
+++ frontend/src/features/map/map-types.ts
@@ -0,0 +1,4 @@
+export interface RouteReq {
+    originCoor: string,
+    destCoor: string,
+}
